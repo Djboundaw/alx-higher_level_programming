8b9f1ca7a7c720e92805b1f72ea9e85b935155c2
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-07 01:36:22 +0000

    some tasks are done

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if a single list has cycle in it
+ *
+ * list: the single list to check
+ * return: 1 if there is cycle, 0 else
+ */
+int check_cycle(listint_t *list)
+{
+}
