6a8cac16e2b7c8febd01c51c83e42bc0c558faf3
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-10 01:41:16 +0000

    task0 - no idea wht's wrong

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Importing the function def add(a, b)"""
     from add_0 import add
 
-    a, b = 1, 2
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
