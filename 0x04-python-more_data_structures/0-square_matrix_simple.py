d97d7cca100b8743288c60d2d3af3521e69ad0a5
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-15 20:05:57 +0000

    line too long - pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-    new_matrix = [[x**2 for x in new_matrix[i]] for i in range(len(new_matrix))]
+    a = len(new_matrix)
+    new_matrix = [[x**2 for x in new_matrix[i]] for i in range(a)]
     return (new_matrix)
