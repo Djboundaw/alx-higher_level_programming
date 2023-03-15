c188c4095027672ad8cc07ebe2afbf06a8de1b5f
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-15 20:03:51 +0000

    task0 _ squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    new_matrix = [[x**2 for x in new_matrix[i]] for i in range(len(new_matrix))]
+    return (new_matrix)
