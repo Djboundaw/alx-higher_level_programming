e59ddcae8a26f2c45eb843075d2709b5aa187af7
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-15 01:04:18 +0000

    task6 done _ handling basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Program handling basic operations"""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    operators = ['+', '-', '*', '/']
+    is_op = [match for match in operators if op in match]
+    if is_op == []:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    match op:
+        case '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        case '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        case '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        case '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
