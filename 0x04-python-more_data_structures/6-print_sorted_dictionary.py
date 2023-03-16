2cfe6526d4f9d9fc0b92e8b0a4431d8db1e88a68
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-16 00:29:28 +0000

    task7 - no return value fixed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    my_keys = list(a_dictionary.items())
-    my_keys.sort()
-    for i in my_keys:
+    for i in sorted(a_dictionary.keys()):
         print("{}: {}".format(i, a_dictionary[i]))
