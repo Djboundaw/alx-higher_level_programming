c383733741c53aab35b46fedce63cb831a0f1903
BondCfall60/69 <bondaw1502@gmail.com>
2023-03-16 01:29:32 +0000

    task10 - Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    max = 0
+    my_key = ""
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > max:
+            max = a_dictionary[i]
+            my_key = i
+    return (my_key)
