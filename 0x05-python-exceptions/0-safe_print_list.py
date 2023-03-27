77c9275d4e9f9a1fa06e33a5f1c6a02dea2d8f7c
Djboundaw <bondaw1502@gmail.com>
2023-03-27 23:50:51 +0000

    task 0 - some mistakes done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -7,6 +7,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-            
     print("")
     return (count)
