32a7531a758ced3abcc6e3be780804c1944793a0
Djboundaw <bondaw1502@gmail.com>
2023-03-29 00:30:38 +0000

    task1 - a square with qize, adding a private unstance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creating a class """
+
+
+class Square():
+    """It's a square"""
+    def __init__(self, size):
+        """Initializing
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
