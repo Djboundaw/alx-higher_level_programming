e2c95422f138c87a32b75f8a43d194ec4645a529
Djboundaw <bondaw1502@gmail.com>
2023-03-29 03:11:38 +0000

    task9 - compares two squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Creating a class """
+
+
+class Square:
+    """It's a square"""
+    def __init__(self, size=0):
+        """Initializing
+        Args:
+            size (int): size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """the getter"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """The setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """The area square"""
+        res = self.__size
+        return (res * res)
+
+    def __le__(self, other):
+        return (self.area() <= other.area())
+
+    def __lt__(self, other):
+        return (self.area() < other.area())
+
+    def __ge__(self, other):
+        return (self.area() >= other.area())
+
+    def __gt__(self, other):
+        return (self.area() > other.area())
+
+    def __eq__(self, other):
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        return (self.area() != other.area())
