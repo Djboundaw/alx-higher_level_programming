78d8130a41aa6a969d825b84eec8bcf9290c6ea3
Djboundaw <bondaw1502@gmail.com>
2023-04-13 16:45:50 +0000

    task7 - pycode style fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,11 +5,10 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+    loadFromJsonFile = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        objects = load_from_json_file("add_item.json")
+        objects = loadFromJsonFile("add_item.json")
     except FileNotFoundError:
         objects = []
     objects.extend(sys.argv[1:])
