9f8ff307f97fed69c8822f836fe4ff546ab07d16
Djboundaw <bondaw1502@gmail.com>
2023-04-13 17:31:05 +0000

    Revert "task9 - using previous function does not work"
    This reverts commit db7ba843e9bb07c6724f360befcdab2e99a90a89.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Creating class student """
-class_to_json = __import__('8-class_to_json').class_to_json
 
 
 class Student:
@@ -20,4 +19,4 @@ class Student:
 
     def to_json(self):
         """Dictionary representation of Student"""
-        return class_to_json(self)
+        return self.__dict__
