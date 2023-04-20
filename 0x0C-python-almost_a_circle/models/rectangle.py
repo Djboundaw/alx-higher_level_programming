52b435327b65e5c133c8a1f05cc96003b433f08d
Djboundaw <bondaw1502@gmail.com>
2023-04-20 00:08:52 +0000

    task7 - display: update for the method display for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,11 @@ class Rectangle(Base):
     def display(self):
         """Public method
         Prints in stdout rectangle instance with the character #"""
+        for k in range(self.__y):
+            print()
         for i in range(self.__height):
+            for e in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
