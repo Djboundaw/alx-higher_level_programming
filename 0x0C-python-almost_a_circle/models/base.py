c4529e0e4166307950072fdf9f612deaab2dd7b8
Djboundaw <bondaw1502@gmail.com>
2023-04-20 03:56:16 +0000

    task15 - dictionary to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Class Base """
+import json
 
 
 class Base:
@@ -17,3 +18,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the json string representation of given argument
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
